9df2e18d12da59cee858d58095248fe2e12ea877
rob smyth <rob@webigence.com>
2021-03-23 15:42:32 +0000

    removed references to slim jquery (updated to full jquery)

---
+++ assets/js/videopopup.js
@@ -0,0 +1,6 @@
+﻿'use strict';
+
+$(document).ready(function () {
+    $('.magnific-video').magnificPopup({ type: 'iframe', mainClass: 'mfp-fade', removalDelay: 200, preloader: true, fixedContentPos: true });
+});
+
