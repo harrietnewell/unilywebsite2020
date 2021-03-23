f6f86ba350c222e54260923a4266913fdce56c73
rob smyth <rob@webigence.com>
2021-03-23 13:12:12 +0000

    moved lightbox into main directory

---
+++ assets/js/hsformlightbox.js
@@ -0,0 +1,71 @@
+﻿'use strict';
+
+var unilyHsFormLightbox = {
+
+    lightboxIdAttr: "data-hs-lightbox-id",
+    openAttr: "data-hs-lightbox-open-id",
+    openSelector: "[data-hs-lightbox-open-id]",
+    closeAttr: "data-hs-lightbox-close-id",
+    closeSelector: "[data-hs-lightbox-close-id]",
+    hsFormLightboxIdAttr: "data-hs-form-lightbox-id",
+    hsFormIdAttr: "data-hs-form-id",
+    hsFormVideoSelector: "data-hs-form-video",
+    
+    init: function init() {
+
+        $(document).on('click', unilyHsFormLightbox.openSelector, function (e) {
+            e.preventDefault();
+            var hsLightboxId = $(this).attr(unilyHsFormLightbox.openAttr);
+            unilyHsFormLightbox.open(hsLightboxId);
+        });
+
+        $(document).on('click', unilyHsFormLightbox.closeSelector, function (e) {
+            e.preventDefault();
+            var hsLightboxId = $(this).attr(unilyHsFormLightbox.closeAttr);
+            unilyHsFormLightbox.close(hsLightboxId);
+        });
+    },
+
+    open: function open(hsLightboxId) {
+        var hsFormId = unilyHsFormLightbox.getHsFormId(hsLightboxId);
+
+        $.ajax({
+            method: 'GET',
+            url: '/umbraco/surface/hsformlightbox/checksubmission/' + hsFormId,
+            success: function success(data) {
+                console.log(data);
+                if (data === 'True') { // If already submitted, play video straight...
+                    $(`[${unilyHsFormLightbox.hsFormLightboxIdAttr}='${hsLightboxId}']`).find(`a[${unilyHsFormLightbox.hsFormVideoSelector}]`).click();
+                } else { // ...otherwise show form
+                    $(`[${unilyHsFormLightbox.lightboxIdAttr}='${hsLightboxId}']`).addClass('opened');
+                }
+            },
+			error: function error() {
+				$(`[${unilyHsFormLightbox.lightboxIdAttr}='${hsLightboxId}']`).addClass('opened');
+			}
+        });
+    },
+
+    close: function close(hsLightboxId) {
+        $(`[${unilyHsFormLightbox.lightboxIdAttr}='${hsLightboxId}']`).removeClass('opened');
+    },
+
+    confirmSubmission: function confirmSubmit(hsLightboxId) {
+
+        var hsFormId = unilyHsFormLightbox.getHsFormId(hsLightboxId);
+        $.ajax({
+            method: 'POST',
+            url: '/umbraco/surface/hsformlightbox/confirmsubmission/' + hsFormId
+        });
+
+        unilyHsFormLightbox.close(hsLightboxId);
+    },
+
+    getHsFormId: function getHsFormId(hsLightboxId) {
+        return $(`[${unilyHsFormLightbox.hsFormLightboxIdAttr}='${hsLightboxId}']`).attr(unilyHsFormLightbox.hsFormIdAttr);
+    }
+};
+
+$(document).ready(function () {
+    unilyHsFormLightbox.init();
+});
--- assets/scss/build_styles.bat
+++ assets/scss/build_styles.bat
@@ -2,4 +2,6 @@ echo "building loading.css" &^
 sass --no-source-map loading.scss ../css/loading.css &^
 echo "building style.css" &^
 sass --no-source-map style.scss ../css/style.css &^
+echo "building hsformlightbox.css" &^
+sass --no-source-map hsformlightbox.scss ../css/hsformlightbox.css &^
 PAUSE
