75da6b8f0d3b8fded994619fcd8be032390c5296
anamarkovic <ana.markovic163@gmail.com>
2021-03-31 12:21:39 +0200

    video popup part 1, small css changes

---
+++ ana/lightbox/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function (){
+    
+    // Open this video in modal
+    $(document).on('click', '.open-video-modal', function (e) {
+        e.preventDefault();
+        var currentVideo = $(this).find('video');
+        var videoModal = $(this).closest('.video-section-wrapper').find('.video-section__modal');
+        var video = videoModal.find('video');
+    
+        currentVideo.prop('muted', true);
+        
+        videoModal.addClass('open');
+        video.prop('muted', false);
+        video.get(0).currentTime = currentVideo.get(0).currentTime;
+        video.get(0).play();
+        console.log('aaa'+currentVideo.get(0).currentTime);
+        
+    })
+    
+    // Close modal and switch playing video
+    $(document).on('click', '.video-section__modal-close', function (e) {
+        e.preventDefault();
+        var mainVideo = $(this).closest('.video-section-wrapper').find('.video-section__cover').find('video');
+        var videoModal = $(this).closest('.video-section__modal');
+        var video = videoModal.find('video');
+    
+        // mainVideo.prop('muted', false);
+        
+        videoModal.removeClass('open');
+        video.prop('muted', true);
+        
+    })
+});
+// if( $("video").prop('muted') )
+//     if( $("video").prop('muted', true) )
