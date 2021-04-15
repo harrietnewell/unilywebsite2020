637bbb7f2a36c63d38b30355784fddd448ce1578
rob smyth <rob@webigence.com>
2021-04-15 15:32:14 +0100

    updated lightbox logic

--- assets/js/videopopup.js
+++ assets/js/videopopup.js
@@ -1,6 +1,19 @@
 ﻿'use strict';
 
 $(document).ready(function () {
-    $('.magnific-video').magnificPopup({ type: 'iframe', mainClass: 'mfp-fade', removalDelay: 200, preloader: true, fixedContentPos: true });
+    $('.magnific-video').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 200,
+        preloader: true,
+        fixedContentPos: true,
+        callbacks: {
+            open: function () {
+                var currentPopup = $.magnificPopup.instance;
+                var customClass = currentPopup.st.el.attr('mfp-custom-class');
+                $(currentPopup.contentContainer).addClass(customClass);
+            }
+        }
+    });
 });
 
