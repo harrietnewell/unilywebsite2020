9146d4991b403d2c5151fcbcc11547cfe73939bf
anamarkovic <ana.markovic163@gmail.com>
2021-04-23 15:45:44 +0200

    display svg after font is loaded

--- assets/js/app.js
+++ assets/js/app.js
@@ -292,5 +292,11 @@ $(document).ready(function () {
 
     }).resize();
     
+    
+    
+});
+// Display svg when document is loaded (e.g. svg after font is loaded)
+document.fonts.ready.then(function() {
+    $('.display-after-font').show();
 });
 
