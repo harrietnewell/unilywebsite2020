0731d2e0203b133517e807b3953d6aef7ada29cc
rob smyth <rob@webigence.com>
2022-05-31 15:35:04 +0100

    updated from source code

--- assets/js/pushpage.js
+++ assets/js/pushpage.js
@@ -3,12 +3,10 @@
 var unilyPushPage = {
 
     pushPageLinkSelector: '[data-push-page-link]',
-    pushPageBlockSelector: '#push-page-block',
     init: function init() { 
 
         $(unilyPushPage.pushPageLinkSelector).click(function () {
             $('html, body').animate({ scrollTop: 0 }, 'slow');
-            $(unilyPushPage.pushPageBlockSelector).find('input[type="text"],input[type="email"]')[0].focus();
         });
     }
 }
