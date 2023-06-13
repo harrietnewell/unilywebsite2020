a0dd7176da56e78c519b763d32ae8460b600bbf4
Rob Smyth <rob.smyth@brightstarr.com>
2023-06-13 15:03:45 +0100

    updated file names for consistency

--- assets/scss/build_styles.bat
+++ assets/scss/build_styles.bat
@@ -4,8 +4,6 @@ echo "building style.css" &^
 sass --no-source-map style.scss ../css/style.css &^
 echo "building hsformlightbox.css" &^
 sass --no-source-map hsformlightbox.scss ../css/hsformlightbox.css &^
-echo "building cards.css" &^
-sass --no-source-map cards.scss ../css/cards.css &^
 echo "building insight.css" &^
 sass --no-source-map insight.scss ../css/insight.css &^
 echo "building report-link.css" &^
@@ -32,4 +30,12 @@ echo "building comparison-table.css" &^
 sass --no-source-map comparison-table.scss ../css/comparison-table.css &^
 echo "building insights-carousel.css" &^
 sass --no-source-map insights-carousel.scss ../css/insights-carousel.css &^
+echo "building uw-testimonials.css" &^
+sass --no-source-map _uw-testimonials.scss ../css/uw-testimonials.css &^
+echo "building uw-block-quote.css" &^
+sass --no-source-map _uw-block-quote.scss ../css/uw-block-quote.css &^
+echo "building uw-content-block.css" &^
+sass --no-source-map _uw-content-block.scss ../css/uw-content-block.css &^
+echo "building uw-cards.css" &^
+sass --no-source-map _uw-cards.scss ../css/uw-cards.css &^
 PAUSE
